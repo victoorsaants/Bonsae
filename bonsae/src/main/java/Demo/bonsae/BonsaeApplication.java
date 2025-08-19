82f96bf28c83746f06fc0ed59fd0be2625ac7ab1
João Victor da Silva Santos <167948689+victoorsaants@users.noreply.github.com>
2025-08-19 19:58:46 -0300

    create bonsae
    arquivo inicial

---
+++ bonsae/src/main/java/Demo/bonsae/BonsaeApplication.java
@@ -0,0 +1,13 @@
+package Demo.bonsae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonsaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonsaeApplication.class, args);
+	}
+
+}
---
+++ bonsae/src/test/java/Demo/bonsae/BonsaeApplicationTests.java
@@ -0,0 +1,13 @@
+package Demo.bonsae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonsaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
